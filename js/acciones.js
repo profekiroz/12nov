e4944463f71ed2a3f809446985dee71dbe8348b4
profekiroz <akiroz99@gmail.com>
2013-11-12 16:42:14 -0600

    Al corriente

---
+++ js/acciones.js
@@ -0,0 +1,41 @@
+// JavaScript Document
+
+$(document).ready(function(e){
+	document.addEventListener("deviceready",function(){
+		$('.btn').tap(function(){
+			var boton = (($(this).attr('class')).split(' '))[1];
+			switch(boton)
+			{
+			
+			case 'b1':
+			navigator.notification.beep(3);
+			break;
+			
+			case 'v1':
+			navigator.notification.vibrate(500);
+			break;
+			
+			
+			
+			}
+		});//tap
+		
+		$('#izquierda').swipeleft(function(){
+			navigator.notification.alert("Deslizo a la izquierda",function(){"Practica4", "Aceptar"});
+			});//barrer izq
+		
+		$('#derecha').swiperight(function(){
+			navigator.notification.confirm("Que quieres hacer ?",function(opt){
+				switch(opt){
+					case 1:
+					break;
+					
+					case 2:
+					
+					break;
+				}// switch opt
+			},"Prueba4","Beep,Vibrar,Cancelar");//confirm
+			
+			})//Barrer derecha
+	},false);//device
+});//document
