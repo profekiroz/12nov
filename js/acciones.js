c85a5e139280f8809894f47d6d2e42448a624e69
Armando Quiroz <akiroz99@gmail.com>
2013-11-14 15:57:32 -0600

    table cordova

--- js/acciones.js
+++ js/acciones.js
@@ -3,9 +3,10 @@
 $(document).ready(function(){
 	document.addEventListener("deviceready",function(){
 		$('#disp table td').eq(1).text()=(device.name);
-			
-			
-			
+		$('#disp table td').eq(3).text()=(device.plataform);	
+		$('#disp table td').eq(5).text()=(device.version);	
+		$('#disp table td').eq(7).text()=(device.cordova);
+		$('#disp table td').eq(9).text()=(device.uuid);	
 			
 	},false);//device
 });//ready
