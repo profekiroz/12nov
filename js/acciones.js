75abc28c30f344cc9c89b459e521e2649c3923ef
Armando Quiroz <akiroz99@gmail.com>
2013-11-14 15:34:05 -0600

    Table

--- js/acciones.js
+++ js/acciones.js
@@ -1,42 +1,11 @@
 // JavaScript Document
 
-$(document).ready(function(e){
+$(document).ready(function(){
 	document.addEventListener("deviceready",function(){
-		$('.btn').tap(function(){
-			var boton = (($(this).attr('class')).split(' '))[1];
-			switch(boton)
-			{
-			
-			case 'b1':
-			navigator.notification.beep(3);
-			break;
-			
-			case 'v1':
-			navigator.notification.vibrate(500);
-			break;
+		$('#disp table td').eq(1).text()=(device.name);
 			
 			
 			
-			}
-		});//tap
-		
-		$('#izquierda').swipeleft(function(){
-			navigator.notification.alert("Deslizo a la izquierda",function(){"Practica4", "Aceptar"});
-			});//barrer izq
-		
-		$('#derecha').swiperight(function(){
-			navigator.notification.confirm("Que quieres hacer ?",function(opt){
-				switch(opt){
-					case 1:
-					navigator.notification.beep(3);
-					break;
-					
-					case 2:	
-					navigator.notification.vibrate(500);				
-					break;
-				}// switch opt
-			},"Prueba4","Beep,Vibrar,Cancelar");//confirm
 			
-			})//Barrer derecha
 	},false);//device
-});//document
+});//ready
