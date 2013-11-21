425d2acede42e904d3609fef59250c65755674f1
Armando Quiroz Cortes <akiroz99@gmail.com>
2013-11-21 15:07:58 -0600

    noname

--- js/acciones.js
+++ js/acciones.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function(){
 	document.addEventListener("deviceready",function(){
-		$('#disp table td').eq(1).text(device.name);
+		$('#disp table td').eq(1).text(device.model);
 		$('#disp table td').eq(3).text(device.platform);	
 		$('#disp table td').eq(5).text(device.version);	
 		$('#disp table td').eq(7).text(device.cordova);
